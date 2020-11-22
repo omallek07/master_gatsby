67f2bfe07543331dbdef17383119428d667fa865
Kevin <omallek@gmail.com>
2020-11-22 11:41:20 -0600

    adds pizza list component and hooks up sanity to graphql in gatsby

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzaPage() {
+export default function PizzaPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>I am the pizza page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        price
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
