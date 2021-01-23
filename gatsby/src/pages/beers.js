62ec4aff1d7738a7b905df9c16fd1c3c63daf7a9
Kevin <omallek@gmail.com>
2021-01-23 15:27:38 -0600

    adds external api -beers to project

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,5 +1,84 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 
-export default function BeersPage() {
-  return <div>Hey I'm the beers page</div>;
+const BeerGridStyles = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeerStyles = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: grid;
+    align-items: center;
+    font-size: 10px;
+  }
+`;
+
+const RatingStyles = styled.div`
+  font-size: 3.5rem;
+  display: flex;
+  .actual {
+    color: var(--yellow);
+  }
+  .total {
+    color: var(--black);
+  }
+  .count {
+    margin-left: 5px;
+    font-size: 2rem;
+  }
+`;
+
+export default function BeersPage({ data }) {
+  return (
+    <>
+      <h2 className="center">
+        We have {data.beers.nodes.length} Beers Available. Dine in Only!
+      </h2>
+      <BeerGridStyles>
+        {data.beers.nodes.map((beer) => {
+          const rating = Math.round(beer.rating.average);
+          return (
+            <SingleBeerStyles key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+              <RatingStyles>
+                <span className="actual">{`*`.repeat(rating)}</span>
+                <span className="total">{`*`.repeat(5 - rating)}</span>
+                <span className="count">({beer.rating.reviews})</span>
+              </RatingStyles>
+            </SingleBeerStyles>
+          );
+        })}
+      </BeerGridStyles>
+    </>
+  );
 }
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
